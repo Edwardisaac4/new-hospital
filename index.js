a8fa539a7e6dac34b5be6c25acef95fe4da57aa3
Edwardisaac4 <isaacdrumz4@gmail.com>
2025-10-14 16:27:01 +0100

    new commit

--- index.js
+++ index.js
@@ -14,19 +14,4 @@ iconContainer.onclick = function() {
     navLinks.classList.toggle('open');
 }
 
-var swiper = new Swiper(".home-slider", {
-    slidesPerView: 1,
-    loop: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+
