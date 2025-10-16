b197487a52d640ba02583a091a65bf618f40b53d
Edwardisaac4 <isaacdrumz4@gmail.com>
2025-10-16 21:35:31 +0100

    added footer and contact us

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-let menuIcon = document.querySelector('.icon i');
-let navLinks = document.querySelector('.navlinks');
-let iconContainer = document.querySelector('.icon');
+let menuIcon = document.querySelector(".icon i");
+let navLinks = document.querySelector(".navlinks");
+let iconContainer = document.querySelector(".icon");
 
+// Hamburger menu functionality
+iconContainer.onclick = function () {
+  if (menuIcon.classList.contains("bx-menu")) {
+    menuIcon.classList.remove("bx-menu");
+    menuIcon.classList.add("bx-x");
+  } else {
+    menuIcon.classList.remove("bx-x");
+    menuIcon.classList.add("bx-menu");
+  }
+  navLinks.classList.toggle("open");
+};
 
-iconContainer.onclick = function() {
-    if (menuIcon.classList.contains('bx-menu')) {
-        menuIcon.classList.remove('bx-menu');
-        menuIcon.classList.add('bx-x');
-    } else {
-        menuIcon.classList.remove('bx-x');
-        menuIcon.classList.add('bx-menu');
-    }
-    navLinks.classList.toggle('open');
-}
+// --- Corrected Slider Functionality ---
 
 
